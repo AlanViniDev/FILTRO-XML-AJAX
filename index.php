dd89762321dda1b13ce445b457983506aa8c5ced
Alan Vinicius <alan123vinicius@gmail.com>
2020-09-22 14:22:18 -0300

    Update index.php

--- index.php
+++ index.php
@@ -37,10 +37,10 @@
 // Variavel gloval. Necessária  para capturar o valor xml retornado no ajax.
 var resultadoxml;
 
-// Variaveis para trabalhar com DOM
+// Variaveis para trabalhar com DOM.
 var parser, xmlDoc;
 
-// Requisição ajax no arquivo XML
+// Requisição ajax no arquivo XML.
 $(document).ready(function() {
     $.ajax({
         type: 'POST',
@@ -59,18 +59,18 @@ parser = new DOMParser();
 
 document.getElementById("veiculos").onchange = function() {
 
-    // Responsavel por capturar o value do select
+    // Responsavel por capturar o value do select.
     var value = document.getElementById("veiculos").value;
 
     
-    // Filtro civic
+    // Filtro civic.
     if (value == 1) {
         document.getElementById("marca").innerHTML = xmlDoc.getElementsByTagName("MARCA")[0].childNodes[0].nodeValue;
         document.getElementById("modelo").innerHTML = xmlDoc.getElementsByTagName("MODELO")[0].childNodes[0].nodeValue;
         document.getElementById("imagem").src = xmlDoc.getElementsByTagName("CAMINHO")[0].childNodes[0].nodeValue;
     }
 
-    // Filtro corolla
+    // Filtro corolla.
     if (value == 2) {
         document.getElementById("marca").innerHTML = xmlDoc.getElementsByTagName("MARCA")[1].childNodes[0].nodeValue;
         document.getElementById("modelo").innerHTML = xmlDoc.getElementsByTagName("MODELO")[1].childNodes[0].nodeValue;
@@ -81,17 +81,17 @@ document.getElementById("veiculos").onchange = function() {
 
 document.getElementById("motocicletas").onchange = function() {
 
-    // Responsavel por capturar o value do select
+    // Responsavel por capturar o value do select.
     var value = document.getElementById("motocicletas").value;
 
-    // Filtro hornet
+    // Filtro hornet.
     if (value == 3) {
         document.getElementById("marca").innerHTML = xmlDoc.getElementsByTagName("MARCA")[2].childNodes[0].nodeValue;
         document.getElementById("modelo").innerHTML = xmlDoc.getElementsByTagName("MODELO")[2].childNodes[0].nodeValue;
         document.getElementById("imagem").src = xmlDoc.getElementsByTagName("CAMINHO")[2].childNodes[0].nodeValue;
     }
 
-    // Filtro r1
+    // Filtro r1.
     if (value == 4) {
         document.getElementById("marca").innerHTML = xmlDoc.getElementsByTagName("MARCA")[3].childNodes[0].nodeValue;
         document.getElementById("modelo").innerHTML = xmlDoc.getElementsByTagName("MODELO")[3].childNodes[0].nodeValue;
